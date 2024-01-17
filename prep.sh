3b13c8391f27b9826df141cd72c49b0b3e536e6d
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2024-01-16 22:12:12 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 module load amber/openmpi/intel/20.11
+obabel -ipdb telaprevir.pdb -h -opdb -O telaprevir_h_2.pdb
+obabel -ipdb telaprevir_h.pdb -omol2 -O telaprevir_obabel.mol2
+antechamber -i telaprevir_obabel.mol2 -fi mol2 -fo mol2 -o telaprevir_antechamber.mol2 -c bcc -s 2
+parmchk2 -i telaprevir_antechamber.mol2 -f mol2 -o telaprevir.frcmod
 pdb4amber -i DnaK_full.pdb -o DnaK_full.amber.pdb --reduce --dry
 tleap -f leap.in
-cpptraj -p dnak_ion.prmtop -i pdb.in>pdb.out
